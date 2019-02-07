539089285b0779024d939c5cb97aea51e62a260b
masterpiece015v <45834706+masterpiece015v@users.noreply.github.com>
2019-02-07 14:49:39 +0900

    20190207001

---
+++ app/src/main/java/tokyo/mp015v/soulnumber/MainActivity.kt
@@ -0,0 +1,50 @@
+package tokyo.mp015v.soulnumber
+
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    var soulnumber : Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Viewの変数化
+        val btn_birthday = findViewById<Button>(R.id.btn_birthday)
+        val btn_comp = findViewById<Button>(R.id.btn_comp)
+        val txt_birthday = findViewById<TextView>(R.id.txt_birthday)
+        val txt_soulnumber = findViewById<TextView>(R.id.txt_soulnumber)
+
+        //イベント登録
+        btn_birthday.setOnClickListener {
+            val dialog = DatePickerDialog(
+                    this,
+                    {view, year, month, dayOfMonth ->
+                        txt_birthday.text = "誕生日:${year}年${month+1}月${dayOfMonth}日"
+
+                        //ソウルナンバーの算出
+                        val strbirthday = year.toString()+(month+1).toString()+dayOfMonth
+                        strbirthday.forEach{
+                            soulnumber = soulnumber + it.toInt() - 48
+                        }
+
+                        if( soulnumber > 10 && soulnumber % 11 != 0 ){
+                            soulnumber = soulnumber / 10 + soulnumber % 10
+                        }
+
+                        txt_soulnumber.text = "ソウルナンバー:${soulnumber}"
+                    }
+            ,2000,1,1
+            )
+            dialog.show()
+        }
+        btn_comp.setOnClickListener {
+            //val inten = Intent(android.R.layout.)
+        }
+    }
+}
